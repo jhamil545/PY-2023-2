d5f368a671831f61a97844d58bc288a3790c3ec1
DarwinPMC <darwin.mamani@upeu.edu.pe>
2023-10-17 19:00:58 -0500

    avance

---
+++ inventario_ganado/src/main/java/upeu/edu/pe/inventario_ganado/InventarioGanadoApplication.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.inventario_ganado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioGanadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioGanadoApplication.class, args);
+	}
+
+}
---
+++ inventario_ganado/src/test/java/upeu/edu/pe/inventario_ganado/InventarioGanadoApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.inventario_ganado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioGanadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
